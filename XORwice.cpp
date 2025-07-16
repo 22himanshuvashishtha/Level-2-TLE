206dc14b221056d107a2256193c5bde332db462f
22himanshuvashishtha <149816813+22himanshuvashishtha@users.noreply.github.com>
2025-07-16 13:04:52 +0530

    Create XORwice.cpp

---
+++ XORwice.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+  long long n,m;
+  cin>>n>>m;
+  int ans=0;
+  ans=(n^m);
+  cout<<ans<<endl;
+}
+int main() 
+{
+    int p;
+    cin>>p;
+    while(p--){
+      solve();
+    }
+    return 0;
+}
