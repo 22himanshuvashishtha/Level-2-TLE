1278ca8a7e6a51a28a7f52978f58d993b0416bf7
22himanshuvashishtha <149816813+22himanshuvashishtha@users.noreply.github.com>
2025-07-31 11:13:40 +0530

    Create x-y ?yes:no .cpp

---
+++ x-y ?yes:no .cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long n,m;
+    cin >> n>>m;
+    if(n-m==1)cout<<"NO"<<endl;
+    else
+    cout << "YES" << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
